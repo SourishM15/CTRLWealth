4ccb00aa6d6a0e37dd3a03edb89360a3c828cd75
SourishM15 <131424980+SourishM15@users.noreply.github.com>
2025-05-27 21:52:32 -0700

    Update Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
-    host: '0.0.0.0',
-    open: true
+    port: 5174,
+    host: true,
+    strictPort: true
   }
 });
